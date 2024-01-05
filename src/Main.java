ed748837a76f9d4dee370dadb6f6ed19dbea43a7
Valknut <ianhogan1922@gmail.com>
2024-01-05 12:59:16 -0800

    Revert "Refactored my Main class."
    This reverts commit 0489055c

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        printHeader();
-        printMessage("Hello World!");
-
-        printHeader();
-        printMessage("By: Ian S.");
-    }
-
-    private static void printMessage(String message)
-    {
-        System.out.println(message);
-    }
-
-    private static void printHeader()
-    {
-        System.out.println("******************************");
+        System.out.println("Hello World!");
+        System.out.println("By: Ian S.");
     }
 }
