0489055c353435867466c36c49ccfa8a6dd64d89
Valknut <ianhogan1922@gmail.com>
2024-01-05 12:36:23 -0800

    Refactored my Main class.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,17 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
-        System.out.println("By: Ian S.");
+        printMessage("Hello World!");
+        printMessage("By: Ian S.");
+    }
+
+    private static void printMessage(String message)
+    {
+        System.out.println(message);
+    }
+
+    public static void printHello()
+    {
+        System.out.println("Hello");
     }
 }
